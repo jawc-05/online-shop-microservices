5fe7653488918351bd20814f51e5054a7f14eba0
jawc-05 <joaoalfredowc@gmail.com>
2026-05-12 13:59:20 +0200

    fix: fixed problems

--- ClientService/src/main/java/br/com/jawc/online/shop/ClientServiceApplication.java
+++ ClientService/src/main/java/br/com/jawc/online/shop/ClientServiceApplication.java
@@ -1,9 +1,12 @@
 package br.com.jawc.online.shop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {
