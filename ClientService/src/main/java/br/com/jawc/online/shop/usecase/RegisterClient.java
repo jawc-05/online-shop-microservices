5b155a6e9889c79a4ab05a266840c804cbbd6c10
jawc-05 <joaoalfredowc@gmail.com>
2026-05-12 15:59:23 +0200

    feat: adding RegisterClient usecase

---
+++ ClientService/src/main/java/br/com/jawc/online/shop/usecase/RegisterClient.java
@@ -0,0 +1,32 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.online.shop.usecase;
+
+import br.com.jawc.online.shop.domain.Client;
+import br.com.jawc.online.shop.repository.IClientRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterClient {
+    private IClientRepository clientRepository;
+
+    @Autowired
+    public RegisterClient(IClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public Client register(@Valid Client client) {
+        return this.clientRepository.insert(client);
+    }
+
+    public Client update(@Valid Client client) {
+        return this.clientRepository.save(client);
+    }
+
+    public void delete(String id){
+        this.clientRepository.deleteById(id);
+    }
+}
