e3f12bc25ba91cade8a38aae79be53d14efa1aad
jawc-05 <joaoalfredowc@gmail.com>
2026-05-14 18:07:00 +0200

    feat: adding RegisterProduct usecase

--- ProductService/src/main/java/br/com/jawc/online/shop/usecase/RegisterProduct.java
+++ ProductService/src/main/java/br/com/jawc/online/shop/usecase/RegisterProduct.java
@@ -3,5 +3,36 @@
  */
 package br.com.jawc.online.shop.usecase;
 
+import br.com.jawc.online.shop.domain.Product;
+import br.com.jawc.online.shop.exception.EntityNotFoundException;
+import br.com.jawc.online.shop.repository.IProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegisterProduct {
+
+    private IProductRepository productRepository;
+
+    @Autowired
+    public RegisterProduct(IProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product registerProduct(@Valid  Product product) {
+        product.setStatus(Product.Status.ACTIVE);
+        return this.productRepository.insert(product);
+    }
+
+    public Product update(@Valid Product product) {
+        return this.productRepository.save(product);
+    }
+
+    public Product delete(String id) {
+        Product prod = productRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException(Product.class, "id", id));
+        prod.setStatus(Product.Status.UNACTIVE);
+        return this.productRepository.save(prod);
+    }
 }
