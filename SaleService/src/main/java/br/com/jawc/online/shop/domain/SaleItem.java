fc52315f9d33422efc1bbd8a9467f7b6f5744de0
jawc-05 <joaoalfredowc@gmail.com>
2026-05-16 00:19:57 +0200

    feat: adding domain pkg in Sale service and adding the SaleItem class

---
+++ SaleService/src/main/java/br/com/jawc/online/shop/domain/SaleItem.java
@@ -0,0 +1,35 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.online.shop.domain;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SaleItem {
+
+    private String productCode;
+    @Builder.Default
+    private Integer quantity = 0;
+    @Builder.Default
+    private BigDecimal priceAtSale = BigDecimal.ZERO;
+    @Builder.Default
+    private BigDecimal totalValue = BigDecimal.ZERO;
+
+
+
+    public void add(Integer quantity) {
+        this.quantity += quantity;
+
+        BigDecimal itemPrice = this.priceAtSale.multiply(BigDecimal.valueOf(quantity));
+
+        this.totalValue = this.totalValue.add(itemPrice);
+    }
+
+}
