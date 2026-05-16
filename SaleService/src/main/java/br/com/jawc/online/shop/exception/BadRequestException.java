9e6378eb95991762152b08dc4965377c3dd8df32
jawc-05 <joaoalfredowc@gmail.com>
2026-05-16 15:06:46 +0200

    feat: adding exception pkg

---
+++ SaleService/src/main/java/br/com/jawc/online/shop/exception/BadRequestException.java
@@ -0,0 +1,13 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.online.shop.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
