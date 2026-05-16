d65fa40f81989ddfb8e17c82cadbaafd25c64fc8
jawc-05 <joaoalfredowc@gmail.com>
2026-05-16 17:54:57 +0200

    feat: add POST endpoints to SaleController

--- SaleService/src/main/java/br/com/jawc/online/shop/controller/SaleController.java
+++ SaleService/src/main/java/br/com/jawc/online/shop/controller/SaleController.java
@@ -13,15 +13,14 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sale")
@@ -81,5 +80,14 @@ public class SaleController {
         return ResponseEntity.ok(searchSale.searchSaleByCode(saleCode));
     }
 
+    @PostMapping
+    @Operation(summary = "register a new sale")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "sale successfully registered"),
+            @ApiResponse(responseCode = "400", description = "Validation error or duplicate key")
+    })
+    public ResponseEntity<Sale> register(@RequestBody @Valid Sale sale){
+        return ResponseEntity.ok(registerSale.registerSale(sale));
+    }
 
 }
