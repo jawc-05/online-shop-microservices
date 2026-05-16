141dadeefce400f050a36a31f84056f24f2f452f
jawc-05 <joaoalfredowc@gmail.com>
2026-05-16 17:39:22 +0200

    feat: adding @EnableFeignClients in SaleServiceApplication

--- SaleService/src/main/java/br/com/jawc/online/shop/SaleServiceApplication.java
+++ SaleService/src/main/java/br/com/jawc/online/shop/SaleServiceApplication.java
@@ -3,9 +3,11 @@ package br.com.jawc.online.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @RefreshScope
+@EnableFeignClients
 public class SaleServiceApplication {
 
 	public static void main(String[] args) {
