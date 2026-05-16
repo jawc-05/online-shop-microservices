d70bcd0cd7432b5cecaeec87f386930838f7edb0
jawc-05 <joaoalfredowc@gmail.com>
2026-05-16 17:38:17 +0200

    feat: adding one more method in sale repository

--- SaleService/src/main/java/br/com/jawc/online/shop/repository/ISaleRepository.java
+++ SaleService/src/main/java/br/com/jawc/online/shop/repository/ISaleRepository.java
@@ -3,7 +3,10 @@
  */
 package br.com.jawc.online.shop.repository;
 
+import br.com.jawc.online.shop.domain.Product;
 import br.com.jawc.online.shop.domain.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
@@ -11,4 +14,7 @@ import java.util.Optional;
 public interface ISaleRepository extends MongoRepository<Sale, String> {
 
     Optional<Sale> findByCode(String code);
+
+    Page<Sale> findAllByStatus(Pageable pageable, Sale.Status status);
+
 }
