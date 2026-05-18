9843691f4824a44fb3e84610e018e45b4e11e9e2
jawc-05 <joaoalfredowc@gmail.com>
2026-05-18 23:15:21 +0200

    fix: fixing error in register method in register usecase in SaleService, to prevent user abuse and to work better

--- SaleService/src/main/java/br/com/jawc/online/shop/usecase/RegisterSale.java
+++ SaleService/src/main/java/br/com/jawc/online/shop/usecase/RegisterSale.java
@@ -5,29 +5,53 @@ package br.com.jawc.online.shop.usecase;
 
 import br.com.jawc.online.shop.domain.Product;
 import br.com.jawc.online.shop.domain.Sale;
+import br.com.jawc.online.shop.dto.SaleDTO;
 import br.com.jawc.online.shop.exception.EntityNotFoundException;
+import br.com.jawc.online.shop.integration.IClientService;
 import br.com.jawc.online.shop.integration.IProductService;
 import br.com.jawc.online.shop.repository.ISaleRepository;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
 
 @Service
 public class RegisterSale {
     private final ISaleRepository saleRepository;
     private final IProductService productService;
+    private final IClientService clientService;
 
     @Autowired
-    public RegisterSale(ISaleRepository saleRepository,  IProductService productService) {
+    public RegisterSale(ISaleRepository saleRepository,  IProductService productService, IClientService clientService) {
         this.saleRepository = saleRepository;
         this.productService = productService;
+        this.clientService = clientService;
     }
 
-    public Sale registerSale(@Valid Sale sale) {
-        sale.setStatus(Sale.Status.STARTED);
+    public Sale registerSale(@Valid SaleDTO saleDTO) {
+        validateCostumer(saleDTO.getClientId());
+
+        Sale sale= Sale.builder()
+                .code(saleDTO.getCode())
+                .clientId(saleDTO.getClientId())
+                .saleDate(saleDTO.getSaleDate())
+                .status(Sale.Status.STARTED)
+                .items(new HashSet<>())
+                .totalValue(BigDecimal.ZERO)
+                .build();
+        sale.recalculateTotalValue();
+
         return saleRepository.insert(sale);
+    }
+
+    private void validateCostumer(@NotNull String clientId) {
+        Boolean isRegistered = clientService.isCustomerRegistered(clientId);
+        if(isRegistered == null || !isRegistered) {
+            throw new EntityNotFoundException(Sale.class,"clientId",  clientId);
+        }
     }
 
     public Sale addProductToSale(@Valid Integer quantity, String productCode, String saleCode) {
@@ -36,6 +60,8 @@ public class RegisterSale {
 
         sale.validateStatus();
         sale.addProduct(product.getCode(), quantity,  product.getPrice());
+
+
 
         return saleRepository.save(sale);
     }
