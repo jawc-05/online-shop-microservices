2261af2988d0523cf66bca181cb1aa09142bdd48
jawc-05 <joaoalfredowc@gmail.com>
2026-05-22 23:38:34 +0200

    refac: changing the feign client name

--- SaleService/src/main/java/br/com/jawc/online/shop/integration/IClientService.java
+++ SaleService/src/main/java/br/com/jawc/online/shop/integration/IClientService.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "client", url = "${app.clientService.url}/client")
+@FeignClient(name = "client-service")
 public interface IClientService {
 
     @GetMapping("/exists/{id}")
--- SaleService/src/main/java/br/com/jawc/online/shop/integration/IProductService.java
+++ SaleService/src/main/java/br/com/jawc/online/shop/integration/IProductService.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product", url = "${app.productService.url}/product")
+@FeignClient(name = "product-service")
 public interface IProductService {
 
     @GetMapping(value = "/code/{code}")
