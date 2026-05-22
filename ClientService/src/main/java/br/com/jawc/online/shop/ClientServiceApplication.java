00bb55efacaa87b4de70a15735b1ee9b94330d67
jawc-05 <joaoalfredowc@gmail.com>
2026-05-22 23:37:10 +0200

    feat: adding the annotation and the import for the services to work using eureka

--- ClientService/src/main/java/br/com/jawc/online/shop/ClientServiceApplication.java
+++ ClientService/src/main/java/br/com/jawc/online/shop/ClientServiceApplication.java
@@ -1,12 +1,12 @@
 package br.com.jawc.online.shop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {
--- ProductService/src/main/java/br/com/jawc/online/shop/ProductServiceApplication.java
+++ ProductService/src/main/java/br/com/jawc/online/shop/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package br.com.jawc.online.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
